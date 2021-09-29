3598f34dcec9b654511e94c64c5e99bcaa56071f
Sreya S Raj <sreyasraj06@gmail.com>
2021-09-29 22:18:51 +0530

    saved!

---
+++ admin/index.php
@@ -0,0 +1,32 @@
+<?php include('partials/menu.php')?>
+<!--main content section section starts here -->
+<div class="main-content">
+<div class="wrapper">
+        <h1>DASHBOARD</h1>
+
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br/>
+            CATEGORIES
+        </div>
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br/>
+            CATEGORIES
+        </div>
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br/>
+            CATEGORIES
+        </div>
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br/>
+            CATEGORIES
+        </div>
+       <div class="clearfix"></div>
+    </div>
+</div>
+<!--main content section ends here-->
+
+<?php include('partials/footer.php')?>
---
+++ admin/partials/footer.php
@@ -0,0 +1,9 @@
+<!--footer section starts here -->
+<div class="footer">
+<div class="wrapper">
+        <p class="text-center">2021 All rights reserved , FOOD BAZAAR . Developed by Sreya S Raj </p>
+    </div>
+</div>
+<!--footer section ends here-->
+</body>
+</html>
